93ec6c822ad496ecdea0ebd510888fd8e9bedeb8
Jeff <jeff2266@gmail.com>
2023-11-15 23:04:55 -0800

    componentize stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Stat from './components/Stat'
 
 function App() {
 	return (
@@ -17,44 +18,12 @@ function App() {
 			<div className="stat-summary grid-flow gap-large section">
 				<h1 className="section-title">Summary</h1>
 				<div className="grid-flow">
-					<div className="stat-wrapper" color="red">
-						<div className="stat-name flex-group">
-							<img src="assets/images/icon-reaction.svg" />
-							<p className="">Reaction</p>
-						</div>
-						<div className="flex-group">
-							<span>80</span> / 100
-						</div>
-					</div>
-					<div className="stat-wrapper" color="orange">
-						<div className="stat-name flex-group">
-							<img src="assets/images/icon-memory.svg" />
-							<p className="">Memory</p>
-						</div>
-						<div className="flex-group">
-							<span>92</span> / 100
-						</div>
-					</div>
-					<div className="stat-wrapper" color="green">
-						<div className="stat-name flex-group">
-							<img src="assets/images/icon-verbal.svg" />
-							<p className="">Verbal</p>
-						</div>
-						<div className="flex-group">
-							<span>61</span> / 100
-						</div>
-					</div>
-					<div className="stat-wrapper" color="blue">
-						<div className="stat-name flex-group">
-							<img src="assets/images/icon-visual.svg" />
-							<p className="">Visual</p>
-						</div>
-						<div className="flex-group">
-							<span>72</span> / 100
-						</div>
-					</div>
+					<Stat name="Reaction" icon="assets/images/icon-reaction.svg" color="red" score={80} />
+					<Stat name="Memory" icon="assets/images/icon-memory.svg" color="orange" score={92} />
+					<Stat name="Verbal" icon="assets/images/icon-verbal.svg" color="green" score={61} />
+					<Stat name="Visual" icon="assets/images/icon-visual.svg" color="blue" score={72} />
 				</div>
-				<button>Continue</button>
+				<button className='button' >Continue</button>
 			</div>
 		</>
 	)
---
+++ src/components/Stat.tsx
@@ -0,0 +1,20 @@
+type Params = {
+	name: string
+	icon: string
+	color: 'red' | 'orange' | 'green' | 'blue'
+	score: number
+}
+
+export default function Stat({ name, icon, color, score }: Params) {
+	return (
+		<div className="stat-wrapper" color={color}>
+			<div className="flex-group">
+				<img src={icon} />
+				<p className="stat-name">{name}</p>
+			</div>
+			<div className="stat-score flex-group">
+				<span>{score}</span> / 100
+			</div>
+		</div>
+	)
+}
