49fe4c8778046812b7179a7902b1f1f6de9a1df1
Jeff <jeff2266@gmail.com>
2023-11-16 22:55:01 -0800

    alt text for images

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -9,7 +9,7 @@ export default function Stat({ name, icon, color, score }: Params) {
 	return (
 		<div className="stat-wrapper" color={color}>
 			<div className="flex-group">
-				<img src={icon} />
+				<img src={icon} alt={name} />
 				<p className="stat-name">{name}</p>
 			</div>
 			<div className="stat-score flex-group">
